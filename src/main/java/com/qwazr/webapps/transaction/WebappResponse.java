c9547a3362b2c1cd0d7a4575605f18d4a8d03dae
Emmanuel Keller <ekeller@open-search-server.com>
2015-10-29 03:19:54 +0100

    Many fixes and improvements

--- src/main/java/com/qwazr/webapps/transaction/WebappResponse.java
+++ src/main/java/com/qwazr/webapps/transaction/WebappResponse.java
@@ -28,245 +28,245 @@ import java.util.TreeMap;
 
 public class WebappResponse implements HttpServletResponse {
 
-    private final HttpServletResponse response;
+	private final HttpServletResponse response;
 
-    private Map<String, Object> variables;
+	private Map<String, Object> variables;
 
-    WebappResponse(HttpServletResponse response) {
-	this.response = response;
-	this.variables = null;
-    }
-
-    public WebappResponse(WebappResponse src) {
-	this.variables = src.variables == null ? null : new TreeMap<String, Object>(src.variables);
-	this.response = src.response;
-    }
-
-    public Map<String, Object> getVariables() {
-	return variables;
-    }
-
-    public WebappResponse variable(String name, Object value) {
-	if (name == null)
-	    return this;
-	if (variables == null)
-	    variables = new TreeMap<String, Object>();
-	if (value == null)
-	    variables.remove(name);
-	else
-	    variables.put(name, value);
-	return this;
-    }
-
-    public WebappResponse setVariable(String name, Object value) {
-	return variable(name, value);
-    }
-
-    @Override
-    public String getCharacterEncoding() {
-	return response.getCharacterEncoding();
-    }
-
-    @Override
-    public String getContentType() {
-	return response.getContentType();
-    }
-
-    @Override
-    public ServletOutputStream getOutputStream() throws IOException {
-	return response.getOutputStream();
-    }
-
-    @Override
-    public PrintWriter getWriter() throws IOException {
-	return response.getWriter();
-    }
-
-    public void sendFile(File file) throws IOException {
-	FileInputStream fis = new FileInputStream(file);
-	try {
-	    BufferedInputStream bis = new BufferedInputStream(fis);
-	    try {
-		IOUtils.copy(bis, response.getOutputStream());
-	    } finally {
-		IOUtils.closeQuietly(bis);
-	    }
-	} finally {
-	    IOUtils.closeQuietly(fis);
+	WebappResponse(HttpServletResponse response) {
+		this.response = response;
+		this.variables = null;
 	}
-    }
 
-    @Override
-    public void setCharacterEncoding(String charset) {
-	response.setCharacterEncoding(charset);
-    }
+	public WebappResponse(WebappResponse src) {
+		this.variables = src.variables == null ? null : new TreeMap<String, Object>(src.variables);
+		this.response = src.response;
+	}
 
-    @Override
-    public void setContentLength(int len) {
-	response.setContentLength(len);
-    }
+	public Map<String, Object> getVariables() {
+		return variables;
+	}
 
-    @Override
-    public void setContentLengthLong(long len) {
-	response.setContentLengthLong(len);
-    }
+	public WebappResponse variable(String name, Object value) {
+		if (name == null)
+			return this;
+		if (variables == null)
+			variables = new TreeMap<String, Object>();
+		if (value == null)
+			variables.remove(name);
+		else
+			variables.put(name, value);
+		return this;
+	}
 
-    @Override
-    public void setContentType(String type) {
-	response.setContentType(type);
-    }
+	public WebappResponse setVariable(String name, Object value) {
+		return variable(name, value);
+	}
 
-    @Override
-    public void setBufferSize(int size) {
-	response.setBufferSize(size);
-    }
+	@Override
+	public String getCharacterEncoding() {
+		return response.getCharacterEncoding();
+	}
 
-    @Override
-    public int getBufferSize() {
-	return response.getBufferSize();
-    }
+	@Override
+	public String getContentType() {
+		return response.getContentType();
+	}
 
-    @Override
-    public void flushBuffer() throws IOException {
-	response.flushBuffer();
-    }
+	@Override
+	public ServletOutputStream getOutputStream() throws IOException {
+		return response.getOutputStream();
+	}
 
-    @Override
-    public void resetBuffer() {
-	response.resetBuffer();
-    }
+	@Override
+	public PrintWriter getWriter() throws IOException {
+		return response.getWriter();
+	}
 
-    @Override
-    public boolean isCommitted() {
-	return response.isCommitted();
-    }
+	public void sendFile(File file) throws IOException {
+		FileInputStream fis = new FileInputStream(file);
+		try {
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			try {
+				IOUtils.copy(bis, response.getOutputStream());
+			} finally {
+				IOUtils.closeQuietly(bis);
+			}
+		} finally {
+			IOUtils.closeQuietly(fis);
+		}
+	}
 
-    @Override
-    public void reset() {
-	response.reset();
-    }
+	@Override
+	public void setCharacterEncoding(String charset) {
+		response.setCharacterEncoding(charset);
+	}
 
-    @Override
-    public void setLocale(Locale loc) {
-	response.setLocale(loc);
-    }
+	@Override
+	public void setContentLength(int len) {
+		response.setContentLength(len);
+	}
 
-    @Override
-    public Locale getLocale() {
-	return response.getLocale();
-    }
+	@Override
+	public void setContentLengthLong(long len) {
+		response.setContentLengthLong(len);
+	}
 
-    @Override
-    public void addCookie(Cookie cookie) {
-	response.addCookie(cookie);
-    }
+	@Override
+	public void setContentType(String type) {
+		response.setContentType(type);
+	}
 
-    @Override
-    public boolean containsHeader(String name) {
-	return response.containsHeader(name);
-    }
+	@Override
+	public void setBufferSize(int size) {
+		response.setBufferSize(size);
+	}
 
-    @Override
-    public String encodeURL(String url) {
-	return response.encodeURL(url);
-    }
+	@Override
+	public int getBufferSize() {
+		return response.getBufferSize();
+	}
 
-    @Override
-    public String encodeRedirectURL(String url) {
-	return response.encodeRedirectURL(url);
-    }
+	@Override
+	public void flushBuffer() throws IOException {
+		response.flushBuffer();
+	}
 
-    @Override
-    @Deprecated
-    public String encodeUrl(String url) {
-	return response.encodeUrl(url);
-    }
+	@Override
+	public void resetBuffer() {
+		response.resetBuffer();
+	}
 
-    @Override
-    @Deprecated
-    public String encodeRedirectUrl(String url) {
-	return response.encodeRedirectUrl(url);
-    }
+	@Override
+	public boolean isCommitted() {
+		return response.isCommitted();
+	}
 
-    @Override
-    public void sendError(int sc, String msg) throws IOException {
-	response.sendError(sc, msg);
-    }
+	@Override
+	public void reset() {
+		response.reset();
+	}
 
-    @Override
-    public void sendError(int sc) throws IOException {
-	response.sendError(sc);
-    }
+	@Override
+	public void setLocale(Locale loc) {
+		response.setLocale(loc);
+	}
 
-    @Override
-    public void sendRedirect(String location) throws IOException {
-	response.sendRedirect(location);
-    }
+	@Override
+	public Locale getLocale() {
+		return response.getLocale();
+	}
 
-    public void redirect(String location) throws IOException {
-	response.sendRedirect(location);
-    }
+	@Override
+	public void addCookie(Cookie cookie) {
+		response.addCookie(cookie);
+	}
 
-    @Override
-    public void setDateHeader(String name, long date) {
-	response.setDateHeader(name, date);
-    }
+	@Override
+	public boolean containsHeader(String name) {
+		return response.containsHeader(name);
+	}
 
-    @Override
-    public void addDateHeader(String name, long date) {
-	response.addDateHeader(name, date);
-    }
+	@Override
+	public String encodeURL(String url) {
+		return response.encodeURL(url);
+	}
 
-    @Override
-    public void setHeader(String name, String value) {
-	response.setHeader(name, value);
-    }
+	@Override
+	public String encodeRedirectURL(String url) {
+		return response.encodeRedirectURL(url);
+	}
 
-    @Override
-    public void addHeader(String name, String value) {
-	response.addHeader(name, value);
-    }
+	@Override
+	@Deprecated
+	public String encodeUrl(String url) {
+		return response.encodeUrl(url);
+	}
 
-    @Override
-    public void setIntHeader(String name, int value) {
-	response.setIntHeader(name, value);
-    }
+	@Override
+	@Deprecated
+	public String encodeRedirectUrl(String url) {
+		return response.encodeRedirectUrl(url);
+	}
 
-    @Override
-    public void addIntHeader(String name, int value) {
-	response.addIntHeader(name, value);
-    }
+	@Override
+	public void sendError(int sc, String msg) throws IOException {
+		response.sendError(sc, msg);
+	}
 
-    @Override
-    public void setStatus(int sc) {
-	response.setStatus(sc);
-    }
+	@Override
+	public void sendError(int sc) throws IOException {
+		response.sendError(sc);
+	}
 
-    @Override
-    @Deprecated
-    public void setStatus(int sc, String sm) {
-	response.setStatus(sc, sm);
+	@Override
+	public void sendRedirect(String location) throws IOException {
+		response.sendRedirect(location);
+	}
 
-    }
+	public void redirect(String location) throws IOException {
+		response.sendRedirect(location);
+	}
 
-    @Override
-    public int getStatus() {
-	return response.getStatus();
-    }
+	@Override
+	public void setDateHeader(String name, long date) {
+		response.setDateHeader(name, date);
+	}
 
-    @Override
-    public String getHeader(String name) {
-	return response.getHeader(name);
-    }
+	@Override
+	public void addDateHeader(String name, long date) {
+		response.addDateHeader(name, date);
+	}
 
-    @Override
-    public Collection<String> getHeaders(String name) {
-	return response.getHeaders(name);
-    }
+	@Override
+	public void setHeader(String name, String value) {
+		response.setHeader(name, value);
+	}
 
-    @Override
-    public Collection<String> getHeaderNames() {
-	return response.getHeaderNames();
-    }
+	@Override
+	public void addHeader(String name, String value) {
+		response.addHeader(name, value);
+	}
+
+	@Override
+	public void setIntHeader(String name, int value) {
+		response.setIntHeader(name, value);
+	}
+
+	@Override
+	public void addIntHeader(String name, int value) {
+		response.addIntHeader(name, value);
+	}
+
+	@Override
+	public void setStatus(int sc) {
+		response.setStatus(sc);
+	}
+
+	@Override
+	@Deprecated
+	public void setStatus(int sc, String sm) {
+		response.setStatus(sc, sm);
+
+	}
+
+	@Override
+	public int getStatus() {
+		return response.getStatus();
+	}
+
+	@Override
+	public String getHeader(String name) {
+		return response.getHeader(name);
+	}
+
+	@Override
+	public Collection<String> getHeaders(String name) {
+		return response.getHeaders(name);
+	}
+
+	@Override
+	public Collection<String> getHeaderNames() {
+		return response.getHeaderNames();
+	}
 
 }
