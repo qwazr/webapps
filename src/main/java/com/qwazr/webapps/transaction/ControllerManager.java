473964c1505f7fe372b7ea22a5d06d2a397d2196
Emmanuel Keller <ekeller@open-search-server.com>
2016-01-18 20:27:15 +0100

    Fix wrong error msg

--- src/main/java/com/qwazr/webapps/transaction/ControllerManager.java
+++ src/main/java/com/qwazr/webapps/transaction/ControllerManager.java
@@ -60,8 +60,8 @@ public class ControllerManager {
 	}
 
 	void handle(WebappTransaction transaction, String controllerPath)
-					throws URISyntaxException, IOException, InterruptedException, ReflectiveOperationException,
-					ServletException, ScriptException, PrivilegedActionException {
+			throws URISyntaxException, IOException, InterruptedException, ReflectiveOperationException,
+			ServletException, ScriptException, PrivilegedActionException {
 		if (controllerPath == null)
 			return;
 		File controllerFile = new File(dataDir, controllerPath);
@@ -93,24 +93,25 @@ public class ControllerManager {
 			// Required for templates
 			pm.add(new FilePermission("<<ALL FILES>>", "read"));
 
-			INSTANCE = new AccessControlContext(new ProtectionDomain[] {
-							new ProtectionDomain(new CodeSource(null, (Certificate[]) null), pm) });
+			INSTANCE = new AccessControlContext(
+					new ProtectionDomain[] { new ProtectionDomain(new CodeSource(null, (Certificate[]) null), pm) });
 		}
 	}
 
 	private void handleFile(WebappTransaction transaction, File controllerFile)
-					throws IOException, ScriptException, PrivilegedActionException, InterruptedException,
-					ReflectiveOperationException, ServletException {
+			throws IOException, ScriptException, PrivilegedActionException, InterruptedException,
+			ReflectiveOperationException, ServletException {
 		String ext = FilenameUtils.getExtension(controllerFile.getName());
 		if (StringUtils.isEmpty(ext))
 			throw new ScriptException("Unsupported controller " + controllerFile.getName());
 		if ("js".equals(ext))
 			handleJavascript(transaction, controllerFile);
-		throw new ScriptException("Unsupported controller extension: " + controllerFile.getName());
+		else
+			throw new ScriptException("Unsupported controller extension: " + controllerFile.getName());
 	}
 
 	private void handleJavascript(WebappTransaction transaction, File controllerFile)
-					throws IOException, ScriptException, PrivilegedActionException {
+			throws IOException, ScriptException, PrivilegedActionException {
 		WebappHttpResponse response = transaction.getResponse();
 		response.setHeader("Cache-Control", "max-age=0, no-cache, no-store");
 		Bindings bindings = scriptEngine.createBindings();
@@ -131,13 +132,12 @@ public class ControllerManager {
 	}
 
 	private void handleJavaClass(WebappTransaction transaction, String className)
-					throws IOException, InterruptedException, ScriptException, ReflectiveOperationException,
-					ServletException {
+			throws IOException, InterruptedException, ScriptException, ReflectiveOperationException, ServletException {
 		IOUtils.CloseableList closeables = new IOUtils.CloseableList();
 		WebappHttpResponse response = transaction.getResponse();
 		response.getVariables().put("closeable", closeables);
 		Class<? extends HttpServlet> servletClass = ClassLoaderUtils
-						.findClass(CompilerManager.getJavaClassLoader(), className);
+				.findClass(CompilerManager.getJavaClassLoader(), className);
 		Objects.requireNonNull(servletClass, "Class not found: " + className);
 		HttpServlet servlet = servletClass.newInstance();
 		try {
